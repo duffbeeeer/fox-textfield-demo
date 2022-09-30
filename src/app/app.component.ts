54547d8ae6bac026aec577141f0ff086328f50c5
Christoph Thiel <duffbeeeer@gmail.coml>
2022-09-30 11:04:36 +0200

    setup demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fox-textfield-demo';
+
+  value = '2022-12-09';
+
+  keyPressed = '';
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    this.keyPressed = event.key;
+  }
+
+  onChange(element: any) {
+    if (!element.value) {
+      this.value = 'empty string';
+    }
+    this.value = element.value;
+    console.log(this.value);
+  }
 }
